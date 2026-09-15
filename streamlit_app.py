deec47c98af9111636cdd2569d2b76ea15b05485
josefinelidenwall1 <josefine.lidenwall@gmail.com>
2026-09-15 10:01:20 +0200

    Convert Snowflake DataFrame to pandas for display
    Added pandas import and converted Snowflake DataFrame to pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests  
+import pandas as pd
 from snowflake.snowpark.functions import col
 from cryptography.hazmat.primitives import serialization
 
@@ -30,7 +31,11 @@ cnx = st.connection("snowflake", private_key=pkb)
 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
