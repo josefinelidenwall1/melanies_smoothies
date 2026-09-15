12012c1c66896f00d46dfa54d3c99b6676253381
josefinelidenwall1 <josefine.lidenwall@gmail.com>
2026-09-15 09:42:45 +0200

    Remove watermelon API call and dataframe display
    Removed redundant API call and data display for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -58,6 +60,4 @@ if ingredients_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
