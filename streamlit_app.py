5bdf2c23615cc41e1ca92ca5add3213bfdb1e94a
josefinelidenwall1 <josefine.lidenwall@gmail.com>
2026-09-15 09:27:51 +0200

    Refactor requests handling in streamlit_app.py
    Updated the requests import and changed the response handling to parse JSON.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests  
 from snowflake.snowpark.functions import col
 from cryptography.hazmat.primitives import serialization
 
@@ -56,6 +57,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
-import requests  
+
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
